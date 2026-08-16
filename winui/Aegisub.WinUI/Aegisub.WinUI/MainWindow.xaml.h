86d9c709872d348a96795f9aebb920ced562b8a8
Zafod <70067552+zafodtom@users.noreply.github.com>
2026-08-16 10:55:30 +0200

    Prepare WinUI workspace for dynamic subtitle sources

--- winui/Aegisub.WinUI/Aegisub.WinUI/MainWindow.xaml.h
+++ winui/Aegisub.WinUI/Aegisub.WinUI/MainWindow.xaml.h
@@ -2,7 +2,7 @@
 
 #include "MainWindow.g.h"
 
-#include <array>
+#include <vector>
 
 namespace winrt::Aegisub_WinUI::implementation
 {
@@ -57,14 +57,20 @@ namespace winrt::Aegisub_WinUI::implementation
             winrt::hstring status;
         };
 
-        std::array<SubtitleRowData, 5> m_rows
-        {{
+        std::vector<SubtitleRowData> m_rows
+        {
             { 143, L"00:12:38.310", L"00:12:41.420", 3.11, L"We still have time.", L"Po\u0159\u00E1d m\u00E1me \u010Das.", L"Schv\u00E1leno" },
             { 144, L"00:12:41.980", L"00:12:43.980", 2.00, L"If the road stays clear, we can still make it.", L"Jestli z\u016Fstane cesta voln\u00E1, po\u0159\u00E1d to stihneme.", L"Schv\u00E1leno" },
             { 145, L"00:12:44.120", L"00:12:46.840", 2.72, L"We should be there before sunrise.", L"M\u011Bli bychom tam b\u00FDt p\u0159ed v\u00FDchodem slunce.", L"Upraveno" },
             { 146, L"00:12:47.050", L"00:12:49.300", 2.25, L"Then we wait for the signal.", L"Pak po\u010Dk\u00E1me na sign\u00E1l.", L"P\u0159ipraveno" },
             { 147, L"00:12:50.100", L"00:12:52.650", 2.55, L"No mistakes this time.", L"Tentokr\u00E1t bez chyb.", L"P\u0159ipraveno" },
-        }};
+        };
+
+        std::vector<winrt::Microsoft::UI::Xaml::Controls::Border> m_rowBorders;
+        std::vector<winrt::Microsoft::UI::Xaml::Controls::TextBlock> m_rowTargetTexts;
+        std::vector<winrt::Microsoft::UI::Xaml::Controls::TextBlock> m_rowStatusTexts;
+        winrt::Microsoft::UI::Xaml::Controls::Grid m_subtitleGrid{ nullptr };
+        winrt::hstring m_sourcePath;
 
         int32_t m_currentIndex{ 2 };
         bool m_loadingSelection{ false };
@@ -75,6 +81,15 @@ namespace winrt::Aegisub_WinUI::implementation
         void UpdateSelectionVisuals();
         void UpdateTableRow(int32_t index);
         int32_t RowIndexFromSender(winrt::Windows::Foundation::IInspectable const& sender) const;
+
+        void InitializeDynamicSubtitleGrid();
+        void RebuildSubtitleGrid();
+        void HookOpenProjectButton();
+        void OpenSourceSubtitleFile();
+        bool LoadSourceSubtitleFile(std::wstring const& filename, std::wstring& errorMessage);
+
+        winrt::Microsoft::UI::Xaml::Controls::Button FindOpenProjectButton(
+            winrt::Microsoft::UI::Xaml::DependencyObject const& root) const;
     };
 }
 
