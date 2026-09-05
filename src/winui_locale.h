426ce77f7b15abe0fdefc62b577d4ea6933260f8
Zafod <70067552+zafodtom@users.noreply.github.com>
2026-09-05 21:40:15 +0200

    Harden WinUI Unicode locale fallback

--- src/winui_locale.h
+++ src/winui_locale.h
@@ -12,6 +12,17 @@ inline bool InitializeTextLocale() {
 #ifdef _WIN32
 	if (std::setlocale(LC_CTYPE, ".UTF-8"))
 		return true;
+#else
+	// CI and development shells often inherit LANG=C even though a UTF-8 locale
+	// is installed. Try common portable names before consulting the environment.
+	if (std::setlocale(LC_CTYPE, "C.UTF-8"))
+		return true;
+	if (std::setlocale(LC_CTYPE, "C.utf8"))
+		return true;
+	if (std::setlocale(LC_CTYPE, "en_US.UTF-8"))
+		return true;
+	if (std::setlocale(LC_CTYPE, "UTF-8"))
+		return true;
 #endif
 	return std::setlocale(LC_CTYPE, "") != nullptr;
 }
